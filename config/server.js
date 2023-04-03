765d9100fa605c36e927efe353d0c69ba73ff05f
rampratapsinghchauhan <rchauhan00@gmail.com>
2023-04-03 22:20:52 +0530

    changed webber tech to elasitic IP

--- config/server.js
+++ config/server.js
@@ -4,5 +4,5 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: env("PUBLIC_URL", "http://webbertech.com"),
+  url: env("PUBLIC_URL", "http://3.19.83.70:1337"),
 });
